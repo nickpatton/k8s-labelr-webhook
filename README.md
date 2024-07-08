75e4e429635ef39d10cae440132d4db554c97055
Nick Patton <nickpatton@live.com>
2024-07-08 07:59:21 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # k8s-labelr-webhook
+
 mutating webhook for automatic labeling of Kubernetes objects
+
+todo: add instructions for deployment with kind cluster
---
+++ main.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	admissionv1 "k8s.io/api/admission/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+)
+
+var (
+	codecs = serializer.NewCodecFactory(runtime.NewScheme())
+	logger = log.New(os.Stdout, "http: ", log.LstdFlags)
+)
+
+func main() {
+	cert, err := tls.LoadX509KeyPair("/certs/cert", "/certs/key")
+	if err != nil {
+		panic(err)
+	}
+
+	logger.Printf("starting webhook server, listening on port 8080")
+	http.HandleFunc("/mutate", mutateObject)
+	server := http.Server{
+		Addr: fmt.Sprintf(":%d", 8080),
+		TLSConfig: &tls.Config{
+			Certificates: []tls.Certificate{cert},
+		},
+		ErrorLog: logger,
+	}
+
+	if err := server.ListenAndServeTLS("", ""); err != nil {
+		panic(err)
+	}
+}
+
+func mutateObject(w http.ResponseWriter, r *http.Request) {
+	logger.Printf("received request on /mutate")
+
+	deserializer := codecs.UniversalDeserializer()
+
+	// Parse the AdmissionReview from the http request.
+	admissionReviewRequest, err := admissionReviewFromRequest(r, deserializer)
+	if err != nil {
+		msg := fmt.Sprintf("error getting admission review from request: %v", err)
+		logger.Printf(msg)
+		w.WriteHeader(400)
+		w.Write([]byte(msg))
+		return
+	}
+
+	logger.Printf("mutation request for object kind: %s", admissionReviewRequest.Request.Resource.Resource)
+	logger.Printf("using admission review request username, %s, to determine appropriate labels to add to object", admissionReviewRequest.Request.UserInfo.Username)
+
+	rawRequest := admissionReviewRequest.Request.Object.Raw
+
+	admissionResponse := &admissionv1.AdmissionResponse{}
+	var patch string
+	patchType := admissionv1.PatchTypeJSONPatch
+
+	// todo: handle resources with a pod template spec and patch those labels as well?
+	switch admissionReviewRequest.Request.Resource.Resource {
+	case "pods":
+		pod := corev1.Pod{}
+		if _, _, err := deserializer.Decode(rawRequest, nil, &pod); err != nil {
+			msg := fmt.Sprintf("error decoding raw pod: %v", err)
+			logger.Printf(msg)
+			w.WriteHeader(500)
+			w.Write([]byte(msg))
+			return
+		}
+		if _, ok := pod.Labels["hello"]; !ok {
+			patch = `[{"op":"add","path":"/metadata/labels","value":{"hello":"world"}}]`
+		}
+	case "configmaps":
+		configmap := corev1.ConfigMap{}
+		if _, _, err := deserializer.Decode(rawRequest, nil, &configmap); err != nil {
+			msg := fmt.Sprintf("error decoding raw configmap: %v", err)
+			logger.Printf(msg)
+			w.WriteHeader(500)
+			w.Write([]byte(msg))
+			return
+		}
+		if _, ok := configmap.Labels["hello"]; !ok {
+			patch = `[{"op":"add","path":"/metadata/labels","value":{"hello":"world"}}]`
+		}
+	default:
+		logger.Printf("unhandled resource type: %s\n", admissionReviewRequest.Request.Resource.Resource)
+	}
+
+	admissionResponse.Allowed = true
+	if patch != "" {
+		admissionResponse.PatchType = &patchType
+		admissionResponse.Patch = []byte(patch)
+	}
+
+	// Construct the response, which is just another AdmissionReview.
+	var admissionReviewResponse admissionv1.AdmissionReview
+	admissionReviewResponse.Response = admissionResponse
+	admissionReviewResponse.SetGroupVersionKind(admissionReviewRequest.GroupVersionKind())
+	admissionReviewResponse.Response.UID = admissionReviewRequest.Request.UID
+
+	resp, err := json.Marshal(admissionReviewResponse)
+	if err != nil {
+		msg := fmt.Sprintf("error marshalling response json: %v", err)
+		logger.Printf(msg)
+		w.WriteHeader(500)
+		w.Write([]byte(msg))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(resp)
+}
+
+func admissionReviewFromRequest(r *http.Request, deserializer runtime.Decoder) (*admissionv1.AdmissionReview, error) {
+	// Validate that the incoming content type is correct.
+	if r.Header.Get("Content-Type") != "application/json" {
+		return nil, fmt.Errorf("expected application/json content-type")
+	}
+
+	// Get the body data, which will be the AdmissionReview
+	// content for the request.
+	var body []byte
+	if r.Body != nil {
+		requestData, err := io.ReadAll(r.Body)
+		if err != nil {
+			return nil, err
+		}
+		body = requestData
+	}
+
+	// Decode the request body into
+	admissionReviewRequest := &admissionv1.AdmissionReview{}
+	if _, _, err := deserializer.Decode(body, nil, admissionReviewRequest); err != nil {
+		return nil, err
+	}
+
+	return admissionReviewRequest, nil
+}
